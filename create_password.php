50cd9a718bbb6d5b21ffd2104f0e263bb22a0573
NuriaMorMag <nuriamormag@gmail.com>
2026-02-11 20:19:53 +0100

    Delete create_password.php

--- create_password.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo password_hash("309403sk", PASSWORD_DEFAULT) ."<br>";
-echo password_hash("o3o3o3", PASSWORD_DEFAULT) ."<br>";
-echo password_hash("12345", PASSWORD_DEFAULT) ."<br>";
-echo password_hash("sdsad", PASSWORD_DEFAULT) ."<br>";
-?>
